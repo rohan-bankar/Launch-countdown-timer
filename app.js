de2c182c3bd39529d7ac321352b15a680231b79d
rohan-bankar <rohanbankar011@gmail.com>
2024-02-24 23:41:20 +0530

    desktop view

---
+++ app.js
@@ -0,0 +1,24 @@
+const countDownDate = new Date("Feb 24, 2024 19:55:00").getTime();
+
+const x = setInterval(()=>{
+
+    const now = new Date().getTime();
+    
+    const distance = countDownDate - now;
+    
+    const day = Math.floor(distance / (1000 * 60 * 60 *24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minute = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const second = Math.floor((distance % (1000 * 60)) / 1000);
+
+    console.log(`Day ${day} : hours ${hours} : minute ${minute} : second ${second}`);
+    document.querySelector(".dayCount").innerHTML = `${day}`;
+    document.querySelector(".hoursCount").innerHTML = `${hours}`;
+    document.querySelector(".minutesCount").innerHTML = `${minute}`;
+    document.querySelector(".secondsCount").innerHTML = `${second}`;
+
+    if(distance < 0){
+        clearInterval(x);
+        document.querySelector(".heading").innerHTML = "Ready To Go";
+    }
+},1000)
